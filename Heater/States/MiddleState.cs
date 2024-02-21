3f9e52cd9944445d4d4fbdc7c3447c1a152ef8f6
kyotakata <57854222+kyotakata@users.noreply.github.com>
2024-02-22 07:47:56 +0900

    20. B17_状態クラスを増やしてもクライアントに影響が出ない

--- Heater/States/MiddleState.cs
+++ Heater/States/MiddleState.cs
@@ -6,22 +6,24 @@ using System.Threading.Tasks;
 
 namespace Heater.States
 {
-    public sealed class LowState : IState
+    public sealed class MiddleState : IState
     {
         public IEnumerable<string> GetCommand()
         {
-            return new List<string> { "Lo", "500W" };
+            return new List<string> { "Mid", "700W" };
         }
 
         public string GetText()
         {
-            return "Lo";
+            return "Mid";
         }
 
         public void UpState(Context context)
         {
-            // Midにしたい
-            context.ChangeState(new MiddleState());
+            // 次はHighになりたい
+            context.ChangeState(new HighState());
+
         }
+
     }
 }
